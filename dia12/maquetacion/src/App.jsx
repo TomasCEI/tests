40d69f9195227d5c08b2e9b576d69fb97c33d9a9
TomichArg <tomich20@gmail.com>
2024-02-13 18:27:12 +0100

    tailwind

--- dia12/maquetacion/src/App.jsx
+++ dia12/maquetacion/src/App.jsx
@@ -3,17 +3,37 @@ import "./App.css";
 import { Header } from "./components/header/Header";
 import { Hero } from "./components/hero/Hero";
 // import { Columns } from "./components/columns/Columns";
-// import { Footer } from "./components/footer/Footer";
+import { Footer } from "./components/footer/Footer";
+import Button from "./components/tailwind/Button";
+import { ScreenFitText } from "./components/tailwind/ScreenFitText";
 
 function App() {
    return (
       <>
          <Header />
          <Hero />
-         {/* <Columns />
-         <Footer /> */}
+         {/* <Columns /> */}
+         <Footer />
+         <Tailwind />
       </>
    );
 }
 
 export default App;
+
+const Tailwind = () => {
+   /**
+    * intersante uso de componentes y animaciones:
+    *
+    * https://www.hover.dev/components/buttons
+    * https://www.framer.com/motion/introduction/
+    * https://www.youtube.com/watch?v=O5lZqqy7VQE
+    */
+   return (
+      <>
+         <h1 className="text-3xl font-bold underline">Hello from Tailwind!</h1>
+         <Button />
+         <ScreenFitText texto="Hola Tomi" />
+      </>
+   );
+};
