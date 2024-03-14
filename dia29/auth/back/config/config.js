e6927acd12512a1fb30cda135d4b2aa7f84d03d5
TomichArg <tomich20@gmail.com>
2024-03-14 08:38:11 +0100

    asd

--- dia29/auth/back/config/config.js
+++ dia29/auth/back/config/config.js
@@ -15,9 +15,9 @@ dotenv.config();
 
 console.log("enviroment es: ",process.env.NODE_ENV || "development");
 
-export const PORT= process.env.PORT ;//        || 3000;
-console.log("PUERTO ES: ", PORT);
+export const PORT= process.env.PORT         || 3000;
 export const domain= process.env.DOMAIN     || "http://localhost"; 
 
+//console.log("PUERTO ES: ", PORT);
 
 export const fullDomain=`${domain}:${PORT}`;
